0b469baa3472f78be1a6e6f0cdab5ed22fc63002
Rex <rexhsu@goodmusic-corp.com>
2017-12-11 13:57:59 +0800

    Initial Commit

---
+++ LineFunctionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LineFunctionDemo
+//
+//  Created by Rex on 2017/12/11.
+//  Copyright © 2017年 cmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LineFunctionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LineFunctionDemo
+//
+//  Created by Rex on 2017/12/11.
+//  Copyright © 2017年 cmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
